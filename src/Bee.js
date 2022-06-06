207d58f3a1431087a5cf29f1cfee165d3e9ed59a
Jmok19927 <jmok19927@gmail.com>
2022-06-06 10:22:37 -0700

    finished beesbeesbees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype); // inherit from Grub
+Bee.prototype.constructor = Bee; //point constructor back to itself
